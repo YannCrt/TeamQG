6393f84cac8ea7039aca465fc49b7431e4cedd1a
YannCrt <yanncretin75@gmail.com>
2024-07-10 11:21:02 +0200

    css videos

--- public_html/view/vueVideo.php
+++ public_html/view/vueVideo.php
@@ -30,37 +30,36 @@ include_once "$racine/view/head.php";
     <?php endif; ?>
 
     <!-- Section pour Afficher les Vidéos -->
-    <h2 class="h2">Liste des Vidéos</h2>
-    <?php if (!empty($videos)): ?>
-        <ul class="ul">
-            <?php foreach ($videos as $video): ?>
-                <li class="li">
-                    <strong class="titre"><?php echo 'titre : '. htmlspecialchars($video['titre']); ?></strong><br>
-                    <small class="description"><?php echo 'description : '. htmlspecialchars($video['description']); ?></small><br>
-                    <!-- Vidéo -->
-                    <?php if (!empty($video['fichier'])): ?>
-                        <video class="video" controls>
-                            <source src="data:video/mp4;base64,<?php echo base64_encode($video['fichier']); ?>" type="video/mp4">
-                            Your browser does not support the video tag.
-                        </video>
-                    <?php endif; ?>
-                    <?php if (isset($is_admin) && $is_admin): ?>
-                    <div class="btn-videos">
-                        <!-- Formulaire de suppression -->
-                        <form action="./?action=suppressionVideo" method="POST">
-                            <input type="hidden" name="id" value="<?php echo $video['id']; ?>">
-                            <button type="submit" class="btn btn-primary btn-video" name="SupprimerVideo" value="Supprimer">Supprimer</button>
-                        </form>
-                        <!-- Lien pour modifier la vidéo -->
-                        <button class="btn btn-primary btn-video"><a href="./?action=updVideo&id=<?php echo $video['id']; ?>">Modifier</a></button>
-                        <?php endif; ?>
-                        <button class="btn btn-secondary btn-video">
-                                <a href="data:video/mp4;base64,<?php echo base64_encode($video['fichier']); ?>" download="<?php echo htmlspecialchars($video['titre']); ?>.mp4">Télécharger</a>
-                        </button>
-                    </div>
-                </li>
-            <?php endforeach; ?>
-        </ul>
+<h2 class="h2">Liste des Vidéos</h2>
+<ul class="ul">
+<?php if (!empty($videos)): ?>
+    <?php foreach ($videos as $video): ?>
+        <li class="li">
+            <strong class="titre"><?php echo 'titre : '. htmlspecialchars($video['titre']); ?></strong><br>
+            <small class="description"><?php echo 'description : '. htmlspecialchars($video['description']); ?></small><br>
+            <!-- Vidéo -->
+            <?php if (!empty($video['fichier'])): ?>
+                <video class="video" controls>
+                    <source src="data:video/mp4;base64,<?php echo base64_encode($video['fichier']); ?>" type="video/mp4">
+                    Your browser does not support the video tag.
+                </video>
+            <?php endif; ?>
+            <?php if (isset($is_admin) && $is_admin): ?>
+            <div class="btn-videos">
+                <!-- Formulaire de suppression -->
+                <form action="./?action=suppressionVideo" method="POST">
+                    <input type="hidden" name="id" value="<?php echo $video['id']; ?>">
+                    <button type="submit" class="btn btn-primary btn-video" name="SupprimerVideo" value="Supprimer">Supprimer</button>
+                </form>
+                <!-- Lien pour modifier la vidéo -->
+                <button class="btn btn-primary btn-video"><a href="./?action=updVideo&id=<?php echo $video['id']; ?>">Modifier</a></button>
+            </div>
+            <?php endif; ?>
+            <button class="btn btn-primary btn-video"><a href="data:video/mp4;base64,<?php echo base64_encode($video['fichier']); ?>" download="<?php echo htmlspecialchars($video['titre']); ?>.mp4">Télécharger</a></button>
+        </li>
+    <?php endforeach; ?>
+</ul>
+
     <?php else: ?>
         <p class="novideo">Aucune vidéo trouvée.</p>
     <?php endif; ?>
